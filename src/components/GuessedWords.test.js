274ff9688b556433aae5e032fdbb413e8c60040a
rmamchyk <roman.mamchyk@gmail.com>
2019-01-19 23:56:18 +0200

    Add GuessedWords component and tests

---
+++ src/components/GuessedWords.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {findByTestAttr, checkProps} from '../../test/testUtils';
+import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+    guessedWords: [{guessedWord: 'train', letterMatchCount: 3}] 
+};
+
+const setup = (props = {}) => {
+    const setupProps = {...defaultProps, ...props};
+    return shallow(<GuessedWords {...setupProps} />);
+};
+
+it('does not throw warning with expected props', () => {
+    checkProps(GuessedWords, defaultProps);
+});
+
+describe('when there are no words guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({guessedWords: []});
+    });
+
+    it('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+
+    it('renders instructions to guess a word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
+        expect(instructions.text().length).not.toBe(0);
+    });
+});
+
+describe('where there are some words guessed', () => {
+    let wrapper;
+    const guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3},
+        {guessedWord: 'agile', letterMatchCount: 1},
+        {guessedWord: 'party', letterMatchCount: 5}
+    ];
+    beforeEach(() => {
+        wrapper = setup({guessedWords});
+    });
+
+    it('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+
+    it('renders `guessed words` section', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        expect(guessedWordsNode.length).toBe(1);
+    });
+
+    it('has correct number of guessed words', () => {
+        const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+        expect(guessedWordNodes.length).toBe(guessedWords.length);
+    });
+});
