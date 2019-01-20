9c8d2abac924ea1471d835ff096d8ab863a49829
rmamchyk <roman.mamchyk@gmail.com>
2019-01-20 14:46:16 +0200

    Set up guessWord action tests

---
+++ src/integration.test.js
@@ -0,0 +1,24 @@
+import {storeFactory} from '../test/testUtils';
+import {guessWord} from './actions';
+
+describe('guessWord action dispatcher', () => {
+    describe('when no guessed words', () => {
+        it('updates state correctly for unsuccessful guess', () => {
+            
+        });
+
+        it('updates store correctly for successful guess', () => {
+            
+        });
+    });
+
+    describe('when some guessed words', () => {
+        it('updates state correctly for unsuccessful guess', () => {
+            
+        });
+
+        it('updates store correctly for successful guess', () => {
+            
+        });  
+    });
+});
