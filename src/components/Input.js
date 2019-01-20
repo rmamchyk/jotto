0e4ca2ebd1067e5aa8b2433c31669b7c8739593a
rmamchyk <roman.mamchyk@gmail.com>
2019-01-20 13:40:45 +0200

    Complete Input component render test case for success=false

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,14 +3,33 @@ import {connect} from 'react-redux';
 
 class Input extends Component {
     render() {
+        const contents = this.props.success 
+            ? null
+            : (
+                <form className="form-inline">
+                    <input 
+                        id="word-guess"
+                        type="text"
+                        placeholder="enter guess"
+                        data-test="input-box" 
+                        className="mb-2 mx-sm-3" />
+                    <button 
+                        type="submit" 
+                        data-test="submit-button" 
+                        className="btn btn-primary mb-2">
+                        Submit
+                    </button>
+                </form>
+            );
+
         return (
-            <div>
-                <button></button>
+            <div data-test="component-input">
+                {contents}
             </div>
         )
     }
 }
 
-export default connect((state) => {
-    return {};
+export default connect(({success}) => {
+    return {success};
 })(Input);
--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -10,16 +10,25 @@ const setup = (initialState={}) => {
 
 describe('render', () => {
     describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = {success: false};
+            wrapper = setup(initialState);
+        });
+
         it('renders component without error', () => {
-            
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1);
         });
 
         it('renders input box', () => {
-            
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(1);
         });
 
         it('renders submit button', () => {
-            
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(1); 
         });
     });
 
